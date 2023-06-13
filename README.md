bb7e4b7a3ea4abd2e98bb56e5cdd57bc1474388a
Chris-Gonz <chris.gonz.fc@gmail.com>
2023-06-13 15:37:47 -0400

    Init commit. Backend Spring/Java. Frontend creat-react-app,

---
+++ README.md
@@ -0,0 +1,21 @@
+# AWS / REACT / JavaScript / JAVA / SPRING
+
+---
+
+## **Backend**
+
+- Java 17 
+- Spring boot
+  - Spring web
+  - Lombok
+- Web Server - Apache Tomcat
+- Maven
+
+## **Frontend**
+- JavaScript
+- React
+- Bootstrap
+
+
+
+
---
+++ src/main/java/com/example/aws_react/AwsReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsReactApplication.class, args);
+    }
+
+}
